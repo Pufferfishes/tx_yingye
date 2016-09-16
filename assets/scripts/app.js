e0240517c007db2fe46bb62ea891b558bdd4417c
sammok2003@hotmail.com <sammok2003@hotmail.com>
2016-09-16 15:34:35 +0800

    Almost finished.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2143,16 +2143,98 @@ document.addEventListener('touchmove', function (e) {
 },true);
 
 var app = {
+    preload: function () {
+        var imgAmounts = $('img').length;
+        var loadedAmounts = 0;
+        var imgBgArr = [
+            { css: '.machine-switch', src: 's01-switch.png'},
+            { css: '.video .bd', src: 'video-bd.jpg'},
+            { css: '.content .bd', src: 's01-content.png'},   
+            { css: '.scene-final .tips', src: 's-final-tips.png'}
+        ];
+
+        imgAmounts += imgBgArr.length;
+
+        $('img').each(function (item) {
+            var originImg = $(this);
+
+            var img = new Image();
+            img.src = originImg.attr('lazy-src');
+            img.onload = function () {
+                originImg.attr('src', this.src);
+                loadedAmounts += 1;
+                start();
+            }
+        });
+
+        for (var i = 0; i < imgBgArr.length; i++) {
+            (function (index) {
+                var img = new Image();
+                img.src = 'http://7xp6iq.com1.z0.glb.clouddn.com/tx_yingye/images/' + imgBgArr[index].src;
+                img.onload = function () {
+                    $(imgBgArr[index].css).css('background-image', 'url(' + this.src + ')');
+                    loadedAmounts += 1;
+                    start();
+                }
+            })(i);
+        }
+
+        function start () {
+            renderNum(Math.floor((loadedAmounts / imgAmounts) * 100));
+
+            if (loadedAmounts == imgAmounts) {
+                setTimeout(function () {
+                    app.create()
+                }, 1000);
+            }
+        }
+
+        function renderNum (num) {
+            var digit = num % 10;
+            var ten = Math.floor(num / 10);
+
+            var hundredDom = $('.loading .hundred');
+            var tenDom = $('.loading .ten');
+            var digitDom = $('.loading .digit');
+
+            if (num == 100) {
+                hundredDom.addClass('num1');
+                tenDom.attr('class', tenDom.attr('class').replace(/ num./g, ' num0'));
+                digitDom.attr('class', digitDom.attr('class').replace(/ num./g, ' num0'));
+            } else {
+                tenDom.attr('class', tenDom.attr('class').replace(/ num./g, '')).addClass('num' + ten);
+                digitDom.attr('class', digitDom.attr('class').replace(/ num./g, '')).addClass('num' + digit);
+            }
+        }
+    },
+
     bindScene01: function () {
         $('.scene01').addClass('active');
 
         setTimeout(function () {
             $('.loading').remove();
+            $('.scene01 .mask').remove();
+
+            $('.machine-switch').on('touchstart', bindVideoPrepare);
+        }, 1400);
+
+        function bindVideoPrepare () {
+            var audioMubu = $('.mubu')[0];
+            audioMubu.play();
+            audioMubu.pause();
+
+            setTimeout(function () {
+                audioMubu.play();
+            }, 1200);
+
+
             $('.scene01').addClass('videoPrepare');
 
             //  prepare content
             bindScene01Content();
-        }, 2000);
+
+            $(this).off('touchstart', bindVideoPrepare);
+        }
 
         function bindScene01Content () {
             var scene01 = $('.scene01');
@@ -2186,10 +2268,10 @@ var app = {
                 //  bind scroll
                 setTimeout(function () {
                     $('.scene01').addClass('viewContent');
-                    scroll = new IScroll('#content', { bounce: false, mouseWheelSpeed: 20 });
+                    scroll = new IScroll('#content', { bounce: true, mouseWheelSpeed: 20 });
 
                     //  go to 快门 scene
-                    // setTimeout(app.bindScene02, 2000);
+                    setTimeout(app.bindScene02, 5000);
                 }, 2000);
             }
         }
@@ -2199,16 +2281,18 @@ var app = {
         $('.scene02').css({'display': 'block'});
 
         setTimeout(function () {
+            $('.scene01').addClass('leave');
             $('.scene02').addClass('active');
 
             //  main scene leave
             setTimeout(function () {
-                $('.main').addClass('leave');
+                $('.scene01').remove();
+                $('.scene02').addClass('leave');
 
                 setTimeout(function () {
                     app.bindFinalScene();
-                }, 2100);
-            }, 2200);
+                }, 300);
+            }, 4400);
         }, 100);
     },
 
@@ -2219,17 +2303,23 @@ var app = {
             $('.main').remove();
         }, 2000);
 
-        $('.scene-final').on('touchstart', function () {
-            $('.scene-final .tips').addClass('active');
+        $('.scene-final .toucharea').on('touchstart', function () {
+            location.reload();
+            // $('.scene-final .tips').addClass('active');
 
-            setTimeout(function () {
-                $('.scene-final .tips').removeClass('active');
-            }, 3000);
+            // setTimeout(function () {
+            //     $('.scene-final .tips').removeClass('active');
+            // }, 3000);
         });
     },
 
     create: function (){
-        setTimeout(start, 2000);
+        $('.loading-num').remove();
+        $('.loading').addClass('prepare');
+
+        setTimeout(function () {
+            start();
+        }, 2000);
 
         function start () {
             $('.loading').addClass('active');
@@ -2240,22 +2330,55 @@ var app = {
     },
 
     start: function (){
-        this.create();
+        this.preload();
     }
 };
 
 $(function (){
     window.onload = window.onresize = function(){
+        var mode = window.innerWidth > 510 ? 'contain' : 'cover';
+
         pageResponse({
             selectors : '.page',     //模块选择器，使用querySelectorAll的方法
-            mode : 'contain',     // auto || contain || cover
+            mode : mode,     // auto || contain || cover
             width : '1080',      //输入页面的宽度，只支持输入数值，默认宽度为320px
             height : '1920'      //输入页面的高度，只支持输入数值，默认高度为504px
-        })
+        });
     };
 
-    // init app
-    app.start();
+    (function () {
+        var loadedAmounts = 0;
+        var imgArr = [
+            { css: '.loading-num', src: 'loading-bg.jpg'},
+            { css: '.loading-num .num', src: 'sprite-num.png'},
+            { css: '.loading-bd', src: 'loading-bg.jpg'},
+            { css: '.scene01 .scene-bd', src: 's01-bg.jpg'}
+        ];
+
+        for (var i = 0; i < imgArr.length; i++) {
+            (function (index) {
+                var img = new Image();
+                img.src = 'http://7xp6iq.com1.z0.glb.clouddn.com/tx_yingye/images/' + imgArr[index].src;
+                img.onload = function () {
+                    $(imgArr[index].css).css('background-image', 'url(' + this.src + ')');
+                    start();
+                }
+            })(i);
+        }
+
+        function start () {
+            loadedAmounts += 1;
+
+            if (loadedAmounts == imgArr.length) {
+                /** init app */
+                setTimeout(function () {
+                    $('.page').show();
+                    app.start();
+                }, 200);
+            }
+        }
+    })();
+
     console.log('app started success...');
 });
 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,6 +8,72 @@ document.addEventListener('touchmove', function (e) {
 },true);
 
 var app = {
+    preload: function () {
+        var imgAmounts = $('img').length;
+        var loadedAmounts = 0;
+        var imgBgArr = [
+            { css: '.machine-switch', src: 's01-switchs.png'},
+            { css: '.machine-switch .bd', src: 's01-switchs-on.png'},
+            { css: '.video .bd', src: 'video-bd.jpg'},
+            { css: '.content .bd', src: 's01-content.png'},   
+            { css: '.scene-final .tips', src: 's-final-tips.png'}
+        ];
+
+        imgAmounts += imgBgArr.length;
+
+        $('img').each(function (item) {
+            var originImg = $(this);
+
+            var img = new Image();
+            img.src = originImg.attr('lazy-src');
+            img.onload = function () {
+                originImg.attr('src', this.src);
+                loadedAmounts += 1;
+                start();
+            }
+        });
+
+        for (var i = 0; i < imgBgArr.length; i++) {
+            (function (index) {
+                var img = new Image();
+                img.src = 'http://7xp6iq.com1.z0.glb.clouddn.com/tx_yingye/images/' + imgBgArr[index].src;
+                img.onload = function () {
+                    $(imgBgArr[index].css).css('background-image', 'url(' + this.src + ')');
+                    loadedAmounts += 1;
+                    start();
+                }
+            })(i);
+        }
+
+        function start () {
+            renderNum(Math.floor((loadedAmounts / imgAmounts) * 100));
+
+            if (loadedAmounts == imgAmounts) {
+                setTimeout(function () {
+                    app.create()
+                }, 1000);
+            }
+        }
+
+        function renderNum (num) {
+            var digit = num % 10;
+            var ten = Math.floor(num / 10);
+
+            var hundredDom = $('.loading .hundred');
+            var tenDom = $('.loading .ten');
+            var digitDom = $('.loading .digit');
+
+            if (num == 100) {
+                hundredDom.addClass('num1');
+                tenDom.attr('class', tenDom.attr('class').replace(/ num./g, ' num0'));
+                digitDom.attr('class', digitDom.attr('class').replace(/ num./g, ' num0'));
+            } else {
+                tenDom.attr('class', tenDom.attr('class').replace(/ num./g, '')).addClass('num' + ten);
+                digitDom.attr('class', digitDom.attr('class').replace(/ num./g, '')).addClass('num' + digit);
+            }
+        }
+    },
+
     bindScene01: function () {
         $('.scene01').addClass('active');
 
@@ -16,9 +82,18 @@ var app = {
             $('.scene01 .mask').remove();
 
             $('.machine-switch').on('touchstart', bindVideoPrepare);
-        }, 1200);
+        }, 1400);
 
         function bindVideoPrepare () {
+            var audioMubu = $('.mubu')[0];
+            audioMubu.play();
+            audioMubu.pause();
+
+            setTimeout(function () {
+                audioMubu.play();
+            }, 1200);
+
+
             $('.scene01').addClass('videoPrepare');
 
             //  prepare content
@@ -43,7 +118,7 @@ var app = {
             function bindShowContent (e) {
                 var curPoint = e.originalEvent.touches[0].pageY;
 
-                if (Math.abs(curPoint - touchStartPoint) >= 100) {
+                if (Math.abs(curPoint - touchStartPoint) >= 30) {
                     scene01
                         .off('touchstart', setTouchStartPoint)
                         .off('touchmove', bindShowContent);
@@ -54,15 +129,15 @@ var app = {
 
             function showContent (e) {
                 $('.scene01 .mask').remove();
-                $('.scene01').addClass('showContent');
+                $('.scene01').addClass('showContent noAnimation');
 
                 //  bind scroll
                 setTimeout(function () {
                     $('.scene01').addClass('viewContent');
-                    scroll = new IScroll('#content', { bounce: false, mouseWheelSpeed: 30 });
+                    scroll = new IScroll('#content', { bounce: true, mouseWheelSpeed: 20 });
 
                     //  go to 快门 scene
-                    // setTimeout(app.bindScene02, 2000);
+                    setTimeout(app.bindScene02, 5000);
                 }, 2000);
             }
         }
@@ -72,16 +147,19 @@ var app = {
         $('.scene02').css({'display': 'block'});
 
         setTimeout(function () {
+            $('.scene01').addClass('leave');
             $('.scene02').addClass('active');
 
             //  main scene leave
             setTimeout(function () {
-                $('.main').addClass('leave');
+                $('.scene01').remove();
+                $('.scene-final').show();
+                $('.scene02').addClass('leave');
 
                 setTimeout(function () {
                     app.bindFinalScene();
-                }, 2500);
-            }, 2200);
+                }, 300);
+            }, 4400);
         }, 100);
     },
 
@@ -92,43 +170,91 @@ var app = {
             $('.main').remove();
         }, 2000);
 
-        $('.scene-final').on('touchstart', function () {
-            $('.scene-final .tips').addClass('active');
+        $('.scene-final .toucharea').on('touchstart', function () {
+            location.reload();
+            // $('.scene-final .tips').addClass('active');
 
-            setTimeout(function () {
-                $('.scene-final .tips').removeClass('active');
-            }, 3000);
+            // setTimeout(function () {
+            //     $('.scene-final .tips').removeClass('active');
+            // }, 3000);
         });
     },
 
     create: function (){
-        setTimeout(start, 2000);
+        $('.loading-num').remove();
+        $('.loading').addClass('prepare');
+
+        setTimeout(function () {
+            start();
+        }, 2000);
 
         function start () {
             $('.loading').addClass('active');
 
             //  show scene 01
-            setTimeout(app.bindScene01, 1600);
+            setTimeout(app.bindScene01, 1650);
         }
     },
 
     start: function (){
-        this.create();
+        this.preload();
     }
 };
 
 $(function (){
     window.onload = window.onresize = function(){
+        var mode = window.innerWidth > 510 ? 'contain' : 'cover';
+
         pageResponse({
             selectors : '.page',     //模块选择器，使用querySelectorAll的方法
-            mode : 'cover',     // auto || contain || cover
+            mode : mode,     // auto || contain || cover
             width : '1080',      //输入页面的宽度，只支持输入数值，默认宽度为320px
             height : '1920'      //输入页面的高度，只支持输入数值，默认高度为504px
-        })
+        });
     };
 
-    // init app
-    app.start();
+    (function () {
+        var loadedAmounts = 0;
+        var imgArr = [
+            { css: '.loading-num', src: 'loading-bg.jpg'},
+            { css: '.loading-num .num', src: 'sprite-num.png'},
+            { css: '.loading-bd', src: 'loading-bg.jpg'},
+            { css: '.scene01 .scene-bd', src: 's01-bg.jpg'}
+        ];
+
+        for (var i = 0; i < imgArr.length; i++) {
+            (function (index) {
+                var img = new Image();
+                img.src = 'http://7xp6iq.com1.z0.glb.clouddn.com/tx_yingye/images/' + imgArr[index].src;
+                img.onload = function () {
+                    $(imgArr[index].css).css('background-image', 'url(' + this.src + ')');
+                    start();
+                }
+            })(i);
+        }
+
+        var img = new Image();
+        var percentImg = $('.loading-num .percent');
+        img.src = percentImg.attr('lazy-src');
+        img.onload = function () {
+            percentImg.attr('src', this.src);
+            start();
+        };
+
+        function start () {
+            loadedAmounts += 1;
+
+            if (loadedAmounts == imgArr.length + 1) {
+
+                /** init app */
+                setTimeout(function () {
+                    $('.page').show();
+                    app.start();
+                }, 200);
+            }
+        }
+    })();
+
     console.log('app started success...');
 });
 
